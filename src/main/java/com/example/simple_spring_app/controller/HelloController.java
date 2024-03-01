88d6d7a10b2d89c0496ba4725c8a2ed1b70c0f5f
hainm <bongnghin5@gmail.com>
2024-03-01 14:28:51 +0700

    change to java 11

--- src/main/java/com/example/simple_spring_app/controller/HelloController.java
+++ src/main/java/com/example/simple_spring_app/controller/HelloController.java
@@ -1,14 +1,17 @@
 package com.example.simple_spring_app.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping
@@ -31,6 +34,74 @@ public class HelloController {
         Map<String, String> map = new HashMap<>();
         map.put("message", "Hello World!");
         map.put("method", "POST");
+        return ResponseEntity.ok(map);
+    }
+
+
+    @GetMapping("/log")
+    public ResponseEntity<?> getLog(@RequestParam int count) {
+        Map<String, String> map = new HashMap<>();
+
+        log.info("Hello World from log!");
+        for (int i = 0; i < count; i++) {
+            log.info("Hello World from log! " + i);
+        }
+        map.put("message", "Hello World from log!");
+        map.put("method", "GET");
+        return ResponseEntity.ok(map);
+    }
+
+
+    // Do the similar for log WARN, ERROR, DEBUG, TRACE
+    @GetMapping("/logWarn")
+    public ResponseEntity<?> getLogWarn(@RequestParam int count) {
+        Map<String, String> map = new HashMap<>();
+
+        log.warn("Hello World from log!");
+        for (int i = 0; i < count; i++) {
+            log.warn("Hello World from log! " + i);
+        }
+        map.put("message", "Hello World from log!");
+        map.put("method", "GET");
+        return ResponseEntity.ok(map);
+    }
+
+    @GetMapping("/logError")
+    public ResponseEntity<?> getLogError(@RequestParam int count) {
+        Map<String, String> map = new HashMap<>();
+
+        log.error("Hello World from log!");
+        for (int i = 0; i < count; i++) {
+            log.error("Hello World from log! " + i);
+        }
+        map.put("message", "Hello World from log!");
+        map.put("method", "GET");
+        return ResponseEntity.ok(map);
+    }
+
+    @GetMapping("/logDebug")
+    public ResponseEntity<?> getLogDebug(@RequestParam int count) {
+        Map<String, String> map = new HashMap<>();
+
+        log.debug("Hello World from log!");
+        for (int i = 0; i < count; i++) {
+            log.debug("Hello World from log! " + i);
+        }
+        map.put("message", "Hello World from log!");
+        map.put("method", "GET");
+        return ResponseEntity.ok(map);
+    }
+
+    @GetMapping("/logTrace")
+    public ResponseEntity<?> getLogTrace(@RequestParam int count) {
+        Map<String, String> map = new HashMap<>();
+
+        log.trace("Hello World from log!");
+        for (int i = 0; i < count; i++) {
+            log.trace("Hello World from log! " + i);
+        }
+        map.put("message", "Hello World from log!");
+        map.put("method", "GET");
         return ResponseEntity.ok(map);
     }
 }
